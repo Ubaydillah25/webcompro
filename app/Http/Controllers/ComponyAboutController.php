6bdb1297992192930dc98e1ea272c37a818c8f49
unknown <ubaydillah.m25@gmail.com>
2026-03-29 20:52:47 +0700

    first commit and auth

---
+++ app/Http/Controllers/ComponyAboutController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ComponyAbout;
+use Illuminate\Http\Request;
+
+class ComponyAboutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ComponyAbout $componyAbout)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ComponyAbout $componyAbout)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ComponyAbout $componyAbout)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ComponyAbout $componyAbout)
+    {
+        //
+    }
+}
